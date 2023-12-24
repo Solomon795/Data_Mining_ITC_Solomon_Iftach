e0a7d274b1b77951f28d11b1ef4f1fcf7980f651
Solomon795 <rebekrebeloff@gmail.com>
2023-12-24 16:32:09 +0300

    Deleted to_mysql.py excessive file

--- to_mysql.py
+++
@@ -1,8 +0,0 @@
-import pymysql
-connection = pymysql.connect(user='root', password='root', database='datamining')
-cursor = connection.cursor()
-cursor.execute("SHOW TABLES")
-print(cursor.fetchall())
-cursor.close()
-connection.close()
-
